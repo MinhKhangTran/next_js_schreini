e8ec353e945a9d043539a16351b9ec6c4cabcb03
MinhKhangTran <minhkhang.tran@gmx.de>
2022-07-06 19:04:50 +0200

    :sparkles: layout

---
+++ components/layout.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Footer from "./footer";
+import Header from "./header";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Schreini</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Eine Website für einen erfundenen Schreiner namens Schreini"
+        />
+        <meta
+          name="keywords"
+          content="Handwerker, Schreiner, NextJs, Bootstrap"
+        />
+        <meta name="author" content="MKT" />
+      </Head>
+      <a className="skip-to-content" href="#main-content">
+        Zum Hauptinhalt springen
+      </a>
+      <Header />
+      <main tabIndex="-1" id="main-content">
+        {props.children}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
